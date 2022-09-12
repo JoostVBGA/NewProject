e787fb9d5e65d040be6397ec34b94ab835651e2f
Joost Verbaarschot <joostheicop@hotmail.com>
2022-09-12 18:57:16 +0300

    Movement Done ?

--- FPMagic/Assets/MyAssets/MyScripts/Player/PlayerController.cs
+++ FPMagic/Assets/MyAssets/MyScripts/Player/PlayerController.cs
@@ -7,17 +7,79 @@ public class PlayerController : MonoBehaviour
 {
     private GameInputs controls;
 
-    private float moveSpeed = 6f;
+    public float moveSpeed = 6f;
 
-    private Vector3 Velocity;
+    private Vector3 velocity;
 
-    private float gravity = -9.81f;
+    public float gravity = -9.81f;
 
     private Vector2 move;
 
-    private float jumpHeight = 2.4f;
+    public float jumpHeight = 2.4f;
 
     private CharacterController controller;
 
-    public Transform Ground
+    public Transform Ground;
+
+    public float distanceToGround = 0.4f;
+
+    public LayerMask groundMask;
+
+    private bool isGrounded;
+
+    private void Awake()
+    {
+        controls = new GameInputs();
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Grav();
+        PlayerMovement();
+        Jump();
+    }
+
+    private void Grav()
+    {
+        isGrounded = Physics.CheckSphere(Ground.position, distanceToGround, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void PlayerMovement()
+    {
+        move = controls.Player.Movement.ReadValue<Vector2>();
+
+        Vector3 movement = (move.y * transform.forward) + (move.x * transform.right);
+
+        controller.Move(movement * moveSpeed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        if (controls.Player.Jump.triggered)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+
 }
